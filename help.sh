04b5ae32818dc1bec1d487974e39b3d08fabc42a
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2020-11-27 22:14:53 +0300

    Update and rename kak.sh to help.sh

--- help.sh
+++ help.sh
@@ -8,3 +8,6 @@ echo "█.
   ███   █▒  █  █░█░   █       █
  ░█ █░  █░ ▓█  █ ░█   ▓▓  █   █
  █▒ ▒█  ▒██▒█  █  ▒█   ███▒   █.     "
+echo "made by pkgsearch"
+echo "bomber– python BOMBER.py"
+echo "ddos– python DDOS.py"
