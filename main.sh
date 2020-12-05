ee5039cc6509c0cdeee628f7a1254c4b1d3f0b8d
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2020-12-06 00:25:13 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,6 +11,6 @@ echo -e "\e[34m
  ░█ █░  █░ ▓█  █ ░█   ▓▓  █   █
  █▒ ▒█  ▒██▒█  █  ▒█   ███▒   █
 made by @pkgsearch \e[0m"
-echo -e "\e[34m ДУДОС [ python DDOS.py ] \e[0m"
-echo -e "\e[34m БОМБЕР [ python BOMBER.py ] \e[0m"
+echo -e "\e[34m ДУДОС [ python ~/px/DDOS.py ] \e[0m"
+echo -e "\e[34m БОМБЕР [ python ~/px/BOMBER.py ] \e[0m"
 echo -e " >>Введите команду:"
