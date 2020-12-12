f6d85e1523272291ca8a03e51170d49a91eaf43a
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2020-12-12 23:13:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,6 +11,4 @@ echo -e "\e[34m
  ░█ █░  █░ ▓█  █ ░█   ▓▓  █   █
  █▒ ▒█  ▒██▒█  █  ▒█   ███▒   █
 made by @pkgsearch \e[0m"
-echo -e "\e[34m ДУДОС [ python ~/px/DDOS.py ] \e[0m"
-echo -e "\e[34m БОМБЕР [ python ~/px/BOMBER.py ] \e[0m"
-echo -e " >>Введите команду:"
+python ~/px/xaker.py
